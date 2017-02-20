6cf2561836533c78b2f252316adc35eddb431899
blackpaopao <woshiwjw11@126.com>
2017-02-20 17:56:00 +0800

    food

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -31,6 +31,21 @@
        <div class="rating">
        	<h1 class="title">商品评价</h1>
        </div>
+       <ratingselect :select-type:"selectType"  :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+       <div class="rating-wrapper">
+       		<ul v-show="food.ratings&& food.ratings.length">
+       			<li v-show="needShow(rating.rateType,rating.text)" v-for="rating in food.ratings" class="rating-item">
+       				<div class="user">
+                <span class="name">{{rating.username}}</span>
+                <img class="avatar" width="12" height="12" :src="rating.avatar">
+              </div>
+              <p class="text">
+                <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+              </p>
+
+       			</li>
+       		</ul>
+       </div>
 
 		</div>
 	</div>
@@ -40,7 +55,10 @@ import cartcontrol from 'components/cartcontrol/cartcontrol';
 import BScroll from 'better-scroll';
 import Vue from 'vue';
 import split from 'components/split/split';
+import ratingselect from 'components/ratingselect/ratingselect';
+  const ALL =2;
   export default{
+
   	props: {
   		food: {
   			type: Object
@@ -48,7 +66,14 @@ import split from 'components/split/split';
   	},
   	data() {
   		return {
-  			showFlag: false
+  			showFlag: false,
+  			selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
   		};
   	},
   	methods: {
@@ -72,11 +97,36 @@ import split from 'components/split/split';
   				return;
   			}
   			Vue.set(this.food, 'count', 1);
+  		},
+  		needShow(type, text) {
+  			if (this.onlyContent && !text) {
+          return false;
+        }
+        if (this.selectType === ALL) {
+          return true;
+        } else {
+          return type === this.selectType;
+        }
   		}
   	},
+  	events: {
+       'ratingtype.select'(type) {
+        this.selectType = type;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      },
+      'content.toggle'(onlyContent) {
+        this.onlyContent = onlyContent;
+        this.$nextTick(() => {
+          this.scroll.refresh();
+        });
+      }
+    },
   	 components: {
       cartcontrol,
-      split
+      split,
+      ratingselect
     }
 
   };
@@ -180,4 +230,5 @@ import split from 'components/split/split';
 				margin-left:18px
 				font-size:14px
 				color:rgb(7,17,27)
+			
 </style>
